16322ca698e16b0676ae4abddd14337a2717f30b
kkordesh <kkordesh@iu.edu>
2022-02-07 19:15:52 -0500

    can get Indy

--- src/Nasa.jsx
+++ src/Nasa.jsx
@@ -1,20 +1,48 @@
+
 import React, { useState, useEffect } from 'react';
 
 
-const baseURL = 'https://api.nasa.gov/planetary/earth/imagery?lon=100.75&lat=1.5&date=2014-02-01&api_key='
+const baseURL = 'https://api.nasa.gov/planetary/earth/imagery'
 const key = '1S6mGpqYmXLPl5kyQlvCibXVZYtq9Rb6hx6qxguF'
-console.log(baseURL)
+
 
 const Nasa = (props) => {
     
-       //const latitude = 20
-      // const longitude = 20 
+    
+    //const [lat, setLat] = useState(null);
+    //const [lon, setLon] = useState(null);
+    //const [status, setStatus] = useState(null);
+    const [results, setResults] = useState([]);
+    
+    
+    /*const getLocation = () => {
+        if (!navigator.geolocation) {
+            setStatus('Your browser does not support Geolocation')
+    } else {
+        setStatus('Locating');
+        navigator.geolocation.getCurrentPosition((position) => {
+            setStatus(null);
+            setLat(position.coords.latitude);
+            setLon(position.coords.longitude);
+        }, () => {
+            setStatus('cannot retrieve location')
+        });
+    }
+} */
 
-  const [results, setResults] = useState([]);
 
-  
-    const fetchResults = () => {
-        fetch("https://api.nasa.gov/planetary/earth/imagery?lon=100.75&lat=1.5&date=2014-02-01&api_key=1S6mGpqYmXLPl5kyQlvCibXVZYtq9Rb6hx6qxguF&dim=.5")
+
+const fetchResults = () => {
+    //const lon = navigator.geolocation.getCurrentPosition((position) => position.coords.longitude);
+    //const lat = navigator.geolocation.getCurrentPosition((position) => position.coords.latitude);
+    //let url =  `${baseURL}?&lon=${lon}&lat=${lat}&date=2014-03-01&api_key=${key}&dim=.5`
+    const lat = 39.768402
+    const lon = -86.158066
+
+    let url =  `https://api.nasa.gov/planetary/earth/imagery?lon=${lon}&lat=${lat}&date=2014-02-01&api_key=${key}&dim=.5`
+    
+    fetch(url)
+    // fetch("https://api.nasa.gov/planetary/earth/imagery?lon=100.75&lat=1.5&date=2014-02-01&api_key=1S6mGpqYmXLPl5kyQlvCibXVZYtq9Rb6hx6qxguF&dim=.5")
   .then(response => response.blob())
   .then(result => setResults(URL.createObjectURL(result)))
   .catch(error => console.log('error', error));
@@ -28,13 +56,10 @@ const Nasa = (props) => {
 
 //    }
 
-
-
    const handleSubmit = (e) => {
    e.preventDefault();
    fetchResults();
    };
-
 
     return ( <div>
         <h1>Hello from nasa</h1>
