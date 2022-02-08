c59bec063f02e38d0c15bd9ec385eb1153c5a7a2
kkordesh <kkordesh@iu.edu>
2022-02-07 21:23:33 -0500

    working geolocation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Nasa from './Nasa'
-import react from 'react'
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const [lat, setLat] = useState('');
+  const [lon, setLon] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude);
+      setLon(position.coords.longitude);
+  }, () => {
+      console.log('cannot retrieve location')
+  });
+  }, []);
+
   return (
     <div>
-     <Nasa /> 
+     <Nasa lat={lat}
+     lon={lon}/> 
     </div>
   );
 }
--- src/Nasa.jsx
+++ src/Nasa.jsx
@@ -2,44 +2,28 @@
 import React, { useState, useEffect } from 'react';
 
 
+
 const baseURL = 'https://api.nasa.gov/planetary/earth/imagery'
 const key = '1S6mGpqYmXLPl5kyQlvCibXVZYtq9Rb6hx6qxguF'
 
 
 const Nasa = (props) => {
-    
-    
-    //const [lat, setLat] = useState(null);
-    //const [lon, setLon] = useState(null);
-    //const [status, setStatus] = useState(null);
+    const {lat, lon} = props
+   
     const [results, setResults] = useState([]);
     
-    
-    /*const getLocation = () => {
-        if (!navigator.geolocation) {
-            setStatus('Your browser does not support Geolocation')
-    } else {
-        setStatus('Locating');
-        navigator.geolocation.getCurrentPosition((position) => {
-            setStatus(null);
-            setLat(position.coords.latitude);
-            setLon(position.coords.longitude);
-        }, () => {
-            setStatus('cannot retrieve location')
-        });
-    }
-} */
-
 
 
 const fetchResults = () => {
     //const lon = navigator.geolocation.getCurrentPosition((position) => position.coords.longitude);
     //const lat = navigator.geolocation.getCurrentPosition((position) => position.coords.latitude);
     //let url =  `${baseURL}?&lon=${lon}&lat=${lat}&date=2014-03-01&api_key=${key}&dim=.5`
-    const lat = 39.768402
-    const lon = -86.158066
+    //const lat = 39.768402
+    //const lon = -86.158066
 
-    let url =  `https://api.nasa.gov/planetary/earth/imagery?lon=${lon}&lat=${lat}&date=2014-02-01&api_key=${key}&dim=.5`
+    
+
+    let url =  `https://api.nasa.gov/planetary/earth/imagery?lon=${lon}&lat=${lat}&date=2021-02-07&api_key=${key}&dim=.5`
     
     fetch(url)
     // fetch("https://api.nasa.gov/planetary/earth/imagery?lon=100.75&lat=1.5&date=2014-02-01&api_key=1S6mGpqYmXLPl5kyQlvCibXVZYtq9Rb6hx6qxguF&dim=.5")
